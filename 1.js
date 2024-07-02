ea29682da9a98c93c8799db720e28c8a2cf2f226
iurypedrosa <iuryppedrosa@protonmail.com>
2024-07-01 22:50:36 -0300

    aprendendo sintaxe e operacoes

---
+++ 1.js
@@ -0,0 +1,19 @@
+var texto = "Oi";
+console.log(texto);
+
+var numero = -1;
+var numero2 = 2.2;
+console.log(numero + numero2);
+
+var booleano = false;
+if (booleano) {
+  console.log("É verdadeiro");
+} else {
+  console.log("É falso");
+}
+var incremento = "11";
+incremento++;
+console.log(incremento);
+
+var nome1 = "João";
+console.log(nome1 + "Da Silva");
