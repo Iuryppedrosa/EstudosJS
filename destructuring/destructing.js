e72fcf44eb8fba1ea2394cd02618b189e95be1cb
Iury Pedrosa <iuryppedrosa@protonmail.com>
2024-09-29 10:35:12 -0300

    Destructuring com objts e arrays

--- destructuring/destructing.js
+++ destructuring/destructing.js
@@ -39,4 +39,35 @@ console.log(d, p);
 let {
   detalhes: { modelo },
 } = produto;
-console.log(modelo, fabricante);
+console.log(modelo);
+
+// descructuring com funcoes parte 3
+let arr = [10, 20, 30, 40, 50];
+function teste([a, b, , c]) {
+  console.log(a, b, c);
+}
+
+teste(arr);
+
+let obj = {
+  a: "jovem",
+  b: "rapaz",
+  c: "homem",
+  d: 40,
+  e: 50,
+};
+
+function teste2({ a, b, e }) {
+  console.log(a, b, e);
+}
+teste2(obj);
+
+// Descructuring com rest operator
+let arr2 = [10, 20, 30, 40, 50];
+let [var1, var2, ...resto] = arr2;
+
+console.log(var1, var2, resto);
+
+let obj2 = { a: 10, b: 20, c: 30, d: 40, e: 50 };
+let { var4, ...resto2 } = obj2;
+console.log(var3, var4, resto2);
